0ed9280112ffadfb1d667e1810c3afcc31eedefa
Thomas Opondo <thomasmopondo@gmail.com>
2024-01-23 05:45:05 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,7 @@
  *Return: The length of the string
  */
 
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	int longit = 0;
 
@@ -17,5 +17,5 @@ int _strlen_recursion(char *s);
 		longit += _strlen_recursion(s + 1);
 	}
 
-	return(longit)
+	return(longit);
 }
