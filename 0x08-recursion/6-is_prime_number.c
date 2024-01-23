fa25a1688151dce26c54ce6a05e41a65fbef9072
Thomas Opondo <thomasmopondo@gmail.com>
2024-01-23 06:09:33 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int actual_prime(int n, int i);
  * Return: 1 if n is a prime number, 0 if not
  */
 
-int is_prime_number(int n);
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
@@ -24,7 +24,7 @@ int is_prime_number(int n);
  * Return: 1 if n is a prime, 0 if not
  */
 
-int actual_prime(int n, int i);
+int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
