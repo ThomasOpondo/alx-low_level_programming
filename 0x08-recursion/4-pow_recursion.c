0a6c224e0bb9d2177d390595e78c92ce7b8afa92
Thomas Opondo <thomasmopondo@gmail.com>
2024-01-23 05:59:41 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,11 @@
  * Return: results of the power
  */
 
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _ pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
